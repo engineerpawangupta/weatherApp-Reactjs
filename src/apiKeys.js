1d9cc794aaddce935ca3b46eca12c4df1c693b13
engineerpawangupta <pawan@radaiansys.com>
2024-09-24 09:25:39 +0530

    api key added

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "42ca930ca5a5676efb4682ea70aae3dd",
   base: "https://api.openweathermap.org/data/2.5/",
 };
